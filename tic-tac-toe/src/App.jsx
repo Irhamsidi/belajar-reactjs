02e75a9bf43636e8e7244b6c561c3e75646f7c71
Irhamsidi <irhamsidipw@gmail.com>
2025-04-13 19:48:52 +0700

    Tic Tac Toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Square from "./components/Square";
+import { use } from "react";
 
 function calculateWinner(squares) {
   const lines = [
@@ -24,9 +25,7 @@ function calculateWinner(squares) {
   return false;
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status = winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O");
 
@@ -36,8 +35,8 @@ function Board() {
     const nextSquares = squares.slice();
 
     nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+
+    onPlay(nextSquares);
   }
 
   return (
@@ -58,4 +57,43 @@ function Board() {
   );
 }
 
-export default Board;
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function jumpTo(netxMove) {
+    setCurrentMove(netxMove);
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = move > 0 ? "Go to move #" + move : "Go to game start";
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
