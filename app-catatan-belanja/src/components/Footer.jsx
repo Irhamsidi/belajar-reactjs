f87ac962f335934db8add6e22e820d92e9cc16dd
Irhamsidi <irhamsidipw@gmail.com>
2025-04-19 10:00:12 +0700

    App Catatan Belanja Finish

--- app-catatan-belanja/src/components/Footer.jsx
+++ app-catatan-belanja/src/components/Footer.jsx
@@ -1,3 +1,13 @@
-export default function Footer() {
-  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
+export default function Footer({ items }) {
+  if (items.length === 0) return <footer className="stats">Daftar belanjaan masih kosong!</footer>;
+
+  const totalItems = items.length;
+  const checkedItems = items.filter((item) => item.checked).length;
+  const percentage = Math.round((checkedItems / totalItems) * 100);
+
+  return (
+    <footer className="stats">
+      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)
+    </footer>
+  );
 }
